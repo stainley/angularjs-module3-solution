a6f2cb1679772a444646f566b13fee71a0c23e0d
Stainley Lebron <saiyamans@gmail.com>
2016-10-03 14:51:13 -0400

    Fixed some bugs.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function () {
     'use strict'
+
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController )
         .service('MenuSearchService', MenuSearchService)
@@ -22,25 +23,41 @@
         return ddo;
     }
 
-    function ItemsDirectiveController(){
-        var list = this;
+     function ItemsDirectiveController(){
+         var list = this;
 
-        list.foundList = function () {
-            for(var i = 0; i < list.items.length; i++){
-
-            }
-        }
-    }
+         list.foundList = function () {
+             for(var i = 0; i < list.items.length; i++){
+                 console.log(list)
+             }
+         }
+     }
 
 
     NarrowItDownController.$inject = ['$scope', 'MenuSearchService']
-    function NarrowItDownController(MenuSearchService) {
-        var ctrl = this;
-        ctrl.searchTerm = '%scope';
+    function NarrowItDownController($scope, MenuSearchService) {
+        var ctrlNarrow = this;
+        ctrlNarrow.found = [];
+        $scope.searchTerm = "";
 
-        ctrl.getMatchedMenuItems = function () {
-            console.log("Finding: " + ctrl.searchTerm);
-            MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+        $scope.foundIt = [];
+
+        this.getMatchedMenuItems = function () {
+
+            ctrlNarrow.found.splice(0, ctrlNarrow.found.length);
+            ctrlNarrow.empty = false;
+
+            if($scope.searchTerm === ""){
+                ctrlNarrow.empty = true;
+            }else{
+                ctrlNarrow.found = MenuSearchService.getMatchedMenuItems($scope.searchTerm);
+                $scope.foundIt = ctrlNarrow.found;
+                console.log($scope.foundIt);
+            }
+        }
+
+        this.removeItem = function(index){
+            ctrlNarrow.found.splice(index,1);
         }
     }
 
@@ -51,31 +68,32 @@
         var service = this;
         var arrayItems = [];
 
-        service.getMatchedMenuItems = function(searchTerm) {
 
+        service.getMatchedMenuItems = function(searchTerm) {
 
             return $http({
                 method: "GET",
                 url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
             }).then( function (result) {
 
-                for(var i=0;i<result.data.menu_items.length; i++){
+                for(var i=0; i<result.data.menu_items.length; i++){
+                    /*console.log(result.data.menu_items[i].description.toLowerCase());*/
                     if (result.data.menu_items[i].description.toLowerCase().indexOf(searchTerm) !== -1){
+                        console.log("Found it");
                         var item={
                             short_name:result.data.menu_items[i].short_name,
                             name:result.data.menu_items[i].name,
                             description:result.data.menu_items[i].description
                         };
+
                         arrayItems.push(item);
                     }
                 }
 
                 service.items = arrayItems;
+                service.founded = arrayItems;
 
-                /*var foundItems = result.data.menu_items.filter(function (item) {
-                    return item.description.toLowerCase().indexOf(selectItem) !== -1;
-                });*/
-                return service.items;
+                return service.founded;
             });
         };
 
