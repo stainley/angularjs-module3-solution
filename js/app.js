925425ba343b93b2241e8bf5e5ffc61a965c521d
Stainley Lebron <saiyamans@gmail.com>
2016-10-02 23:50:29 -0400

    Fixed Services

--- js/app.js
+++ js/app.js
@@ -2,15 +2,44 @@
     'use strict'
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController )
-        .service('MenuSearchService', MenuSearchService);
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItemsDirective)
+    ;
+
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItemTemplate.html',
+            restrict: 'E',
+            scope: {
+                items: '<',
+                onRemove: '&'
+            },
+            controller: ItemsDirectiveController,
+            controllerAs: 'ctrl',
+            bindToController: true
+        };
+
+        return ddo;
+    }
+
+    function ItemsDirectiveController(){
+        var list = this;
+
+        list.foundList = function () {
+            for(var i = 0; i < list.items.length; i++){
+
+            }
+        }
+    }
 
 
     NarrowItDownController.$inject = ['MenuSearchService']
     function NarrowItDownController(MenuSearchService) {
-        var menu = this;
+        var ctrl = this;
+        ctrl.searchTerm = "";
 
-        var promise = MenuSearchService.getMatchedMenuItems();
-
+        ctrl.searchTerm = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+        console.log(ctrl.searchTerm);
     }
 
 
@@ -18,20 +47,38 @@
     function MenuSearchService($http) {
 
         var service = this;
+        var arrayItems = [];
 
-        service.getMatchedMenuItems = function() {
+        service.getMatchedMenuItems = function(searchTerm) {
+
+
             return $http({
                 method: "GET",
                 url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-                /*params: {
-                    category: searchTerm
-                }*/
             }).then( function (result) {
-                var foundItems;
 
-                console.log(result.data);
-                return foundItems;
+                for(var i=0;i<result.data.menu_items.length; i++){
+                    if (result.data.menu_items[i].description.toLowerCase().indexOf(searchTerm) !== -1){
+                        var item={
+                            short_name:result.data.menu_items[i].short_name,
+                            name:result.data.menu_items[i].name,
+                            description:result.data.menu_items[i].description
+                        };
+                        arrayItems.push(item);
+                    }
+                }
+
+                service.items = arrayItems;
+
+                /*var foundItems = result.data.menu_items.filter(function (item) {
+                    return item.description.toLowerCase().indexOf(selectItem) !== -1;
+                });*/
+                return service.items;
             });
+        };
+
+        service.remove = function (index) {
+            service.foundItems.splice(index, 1);
         };
     }
 
